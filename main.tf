eacc0e7e6a23d5d4029e8c6cc571d4e70a3a0e9a
manmohanmirkar123 <manmohanmirkar@gmail.com>
2023-02-17 16:22:59 +0530

    intial commit

---
+++ main.tf
@@ -0,0 +1,104 @@
+provider "azurerm" {
+
+    features {}
+     skip_provider_registration = true
+}
+
+data "azurerm_resource_group" "example" {
+  name = "1-a3f9d3b0-playground-sandbox"
+}
+
+terraform {
+  backend "azurerm" {
+    resource_group_name  = data.azurerm_resource_group.example.name
+    storage_account_name = azurerm_storage_account.example.name
+    container_name       = "tfstate"
+    key                  = "prod.terraform.tfstate"
+  }
+}
+
+
+
+output "id" {
+  value = data.azurerm_resource_group.example.id
+}
+
+
+resource "azurerm_virtual_network" "main" {
+  name                = "demo-network"
+  address_space       = ["10.0.0.0/16"]
+  location            = data.azurerm_resource_group.example.location
+  resource_group_name = data.azurerm_resource_group.example.name
+}
+
+resource "azurerm_subnet" "internal" {
+  name                 = "internal"
+  resource_group_name  = data.azurerm_resource_group.example.name
+  virtual_network_name = azurerm_virtual_network.main.name
+  address_prefixes     = ["10.0.2.0/24"]
+}
+
+resource "azurerm_public_ip" "public_ip" {
+  name                = "vm_public_ip"
+  resource_group_name = data.azurerm_resource_group.example.name
+  location            = data.azurerm_resource_group.example.location
+  allocation_method   = "Dynamic"
+}
+
+resource "azurerm_network_interface" "main" {
+  name                = "demo-nic"
+  location            = data.azurerm_resource_group.example.location
+  resource_group_name = data.azurerm_resource_group.example.name
+
+  ip_configuration {
+    name                          = "testconfiguration1"
+    subnet_id                     = azurerm_subnet.internal.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id = azurerm_public_ip.public_ip.id
+  }
+}
+
+
+resource "azurerm_virtual_machine" "main" {
+  name                  = "demo-vm"
+  location              = data.azurerm_resource_group.example.location
+  resource_group_name   = data.azurerm_resource_group.example.name
+  network_interface_ids = [azurerm_network_interface.main.id]
+  vm_size               = "Standard_DS1_v2"
+
+  # Uncomment this line to delete the OS disk automatically when deleting the VM
+  # delete_os_disk_on_termination = true
+
+  # Uncomment this line to delete the data disks automatically when deleting the VM
+  # delete_data_disks_on_termination = true
+
+  storage_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "16.04-LTS"
+    version   = "latest"
+  }
+  storage_os_disk {
+    name              = "myosdisk1"
+    caching           = "ReadWrite"
+    create_option     = "FromImage"
+    managed_disk_type = "Standard_LRS"
+  }
+  os_profile {
+    computer_name  = "hostname"
+    admin_username = "testadmin"
+    admin_password = "Password1234!"
+  }
+  os_profile_linux_config {
+    disable_password_authentication = false
+  }
+  tags = {
+    environment = "staging"
+  }
+}
+
+output "network_interface_private_ip" {
+  description = "private ip addresses of the vm nics"
+  value       = azurerm_public_ip.public_ip.ip_address
+  
+}
